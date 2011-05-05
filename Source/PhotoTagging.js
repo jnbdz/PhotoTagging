f7014095618dffd3497885279b4e04756d7cfc4e
Jean-Nicolas Boulay Desjardins <jnbdzjnbdz@gmail.com>
2011-05-05 15:47:30 -0400

    first commit

---
+++ Source/PhotoTagging.js
@@ -0,0 +1,19 @@
+/*
+---
+description: With this plugin you can tag people and/or objects in pictures.
+
+authors:
+  - Jean-Nicolas Boulay Desjardins (http://jean-nicolas.name)
+
+license:
+  - MIT-style license
+
+requires:
+ - core/1.3:   '*'
+
+provides:
+  - PhotoTagging
+...
+*/
+
+
